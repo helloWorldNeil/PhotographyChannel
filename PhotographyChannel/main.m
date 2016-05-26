c7cbaa62a5909f0874c2667aba1d828f07d378ad
一线天司马亮 <hotman6662008@126.com>
2016-05-26 10:34:23 +0800

    Initial Commit

---
+++ PhotographyChannel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PhotographyChannel
+//
+//  Created by Neil Wang on 16/5/26.
+//  Copyright © 2016年 Neil Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ PhotographyChannelTests/PhotographyChannelTests.m
@@ -0,0 +1,39 @@
+//
+//  PhotographyChannelTests.m
+//  PhotographyChannelTests
+//
+//  Created by Neil Wang on 16/5/26.
+//  Copyright © 2016年 Neil Wang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PhotographyChannelTests : XCTestCase
+
+@end
+
+@implementation PhotographyChannelTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
